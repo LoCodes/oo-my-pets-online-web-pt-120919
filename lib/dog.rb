d6d8d531615c40b81b2c5ba2e503f6c29cf79fd6
Loren Cipriano <loren25cipriano@gmail.com>
2020-02-01 01:56:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
   # code goes here
+  attr_accessor :mood, :owner
+  attr_reader :name
+
+  @all = []
+
+
+  def initialize(name, owner)
+    @name = name
+    @ownder = owner
+    @mood = "nervous"
+    @@all << self
+  end
+
+
 end
