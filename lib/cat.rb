4c74117bb1777db03e0fd887d6f6f73191a06bae
Loren Cipriano <loren25cipriano@gmail.com>
2020-02-01 01:40:45 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,11 +1,11 @@
 class Cat
   # code goes here
   attr_reader :name
-  attr_accessor :owner, :mood 
+  attr_accessor :owner, :mood
 
   def initialize(name)
     @name = name
-    @owner = owner 
+    @owner = owner
     @mood = "playful"
   end
 
